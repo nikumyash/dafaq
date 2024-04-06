e530b56260f86fb1b37ca5896af0e9205715191f
nikumyash <98515855+nikumyash@users.noreply.github.com>
2024-04-06 08:59:03 +0530

    before auth setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import HomePage from './pages/HomePage'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import AuthProviderTeachers from "./components/AuthProviderTeachers"
+import AuthProviderStudents from "./components/AuthProviderStudents"
 
 const App = () => {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <AuthProviderTeachers>
+            <Route path="/teacher" element={<HomePage/>}/>
+          </AuthProviderTeachers>
+          <AuthProviderStudents>
+            <Route path="/student" element={<HomePage/>}/>
+          </AuthProviderStudents>
+        </Routes>
+      </BrowserRouter>
       <HomePage/>
     </>
   )
