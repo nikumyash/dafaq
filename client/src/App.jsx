078c69d56043ca9445529adb34faf54053e156cc
nikumyash <98515855+nikumyash@users.noreply.github.com>
2024-04-06 09:15:45 +0530

    fixed bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,17 @@ import HomePage from './pages/HomePage'
 import {BrowserRouter,Route,Routes} from "react-router-dom"
 import AuthProviderTeachers from "./components/AuthProviderTeachers"
 import AuthProviderStudents from "./components/AuthProviderStudents"
+import TeacherPage from './pages/TeacherPage'
+import StudentPage from './pages/StudentPage'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <AuthProviderTeachers>
-            <Route path="/teacher" element={<HomePage/>}/>
-          </AuthProviderTeachers>
-          <AuthProviderStudents>
-            <Route path="/student" element={<HomePage/>}/>
-          </AuthProviderStudents>
+          <Route index element={<HomePage/>}/>
+            <Route path="/teacher" element={<AuthProviderTeachers><TeacherPage/></AuthProviderTeachers>}/>
+            <Route path="/student" element={<AuthProviderStudents><StudentPage/></AuthProviderStudents>}/>
         </Routes>
       </BrowserRouter>
       <HomePage/>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+
 const Navbar = () => {
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0();
+
   return (
     <div className="flex justify-between items-center h-16 px-8 m-auto text-white bg-zinc-900 ">
         <h1 className='text-3xl ml-6 font-bold text-orange-600'>
@@ -12,7 +18,14 @@ const Navbar = () => {
               For teachers
             </li>
         </ul>   
-
+        <div className="flex flex-row gap-8 mr-8">
+            <div className="flex rounded-md transition-all ease duration-200 cursor-pointer h-12 w-32 items-center justify-center bg-blue-600 text-white text-lg hover:bg-blue-700">
+              Register
+            </div>
+            <div className="flex rounded-md transition-all ease duration-200 cursor-pointer h-12 w-28 items-center justify-center hover:bg-slate-800 hover:text-white">
+              Log In 
+            </div>
+        </div>
     </div>  
   )
 }
