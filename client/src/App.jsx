5ebb7eb2517bc0cd88fb2f627d0c4fd3d8ca5524
VedantPawashe <vedantpawshe2204@gmail.com>
2024-04-06 20:07:18 +0530

    prefinal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import ProfilePage from './pages/SignUpPage'
+import CreateClass from './pages/CreateClass'
+import ClassCard from './components/ClassCard'
+import Classroom from './pages/Classroom'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import DoubtsMgmt from './pages/DoubtsMgmt'
+import Doubt from './components/Doubt'
 
-import HomePage from './pages/HomePage'
-import {BrowserRouter,Route,Routes} from "react-router-dom"
-import AuthProviderCustom from './components/AuthProviderCustom'
 
 const App = () => {
   return (
     <>
-
-      <AuthProviderCustom>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<HomePage/>}/>
-          </Routes>
-        </BrowserRouter>
-      </AuthProviderCustom>
-
+      <div className="flex">
+        <DoubtsMgmt/>
+        <Doubt/>
+      </div>
     </>
   )
 }
